ef5ef13a5eb90cd4c3d6df94bce3065d1ac5bf99
Dmitry Kireev <dimactek2014@ya.ru>
2016-09-30 08:54:25 +0400

    dob transist

--- metro.cc
+++ metro.cc
@@ -6,8 +6,17 @@ using namespace std;
 
 int
 main() {
-    cout <<read_word(cin) <<endl;
-    Scheme scheme = parse_scheme(cin);
-    cout << scheme << endl;
-    return 0;
+   // cout <<read_word(cin) <<endl;
+   // Scheme scheme = parse_scheme(cin);
+   // cout << scheme << endl;
+    //return 0;
+    Station s;
+    s.transits.first=0;
+    s.transits.last=0;
+    s.name="station";
+    add_transit(&s, &s);
+    add_transit(&s, &s);
+    add_transit(&s, &s);
+    cout << s << endl;
+
 }
--- transit.cc
+++ transit.cc
@@ -5,4 +5,13 @@ using namespace std;
 void
 add_transit(Station* station, Station* destination) {
     // TODO: добавить переход со станции `station` на станцию `destination`.
+    Transit*transit=new Transit;
+    transit->next=nullptr;
+    transit->destination=destination;
+    if (station->transits.last != nullptr )
+        station->transits.last->next=transit;
+    else station->transits.first=transit;
+    station->transits.last=transit;
+
 }
+
